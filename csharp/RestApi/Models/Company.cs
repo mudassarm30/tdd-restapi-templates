4d1cb0ef93033215d47b8f5d63647eb782352295
Mudassar Majeed <mudassarm30@gmail.com>
2023-11-24 02:38:17 +0100

    Fixed CSharp project

---
+++ csharp/RestApi/Models/Company.cs
@@ -0,0 +1,12 @@
+namespace RestApi.Models;
+
+public class Company
+{
+    public DateTime Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
