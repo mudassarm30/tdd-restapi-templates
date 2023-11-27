ea426ce99fed4e7a046ccde74b63b78fca5f7cba
Mudassar Majeed <mudassarm30@gmail.com>
2023-11-27 23:09:36 +0100

    Fixed Lambda integration

---
+++ csharp/RestApi/LambdaEntryPoint.cs
@@ -0,0 +1,13 @@
+using Amazon.Lambda.AspNetCoreServer;
+using Microsoft.AspNetCore.Hosting;
+
+namespace RestApi
+{
+    public class LambdaEntryPoint : APIGatewayProxyFunction
+    {
+        protected override void Init(IWebHostBuilder builder)
+        {
+            builder.UseStartup<Startup>();
+        }
+    }
+}
--- csharp/RestApi/Program.cs
+++ csharp/RestApi/Program.cs
@@ -9,7 +9,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
-builder.Services.AddAWSLambdaHosting(LambdaEventSource.HttpApi);
 builder.Services.Configure<MongoDBConfig>(builder.Configuration.GetSection("MongoDB"));
 builder.Services.AddSingleton<IMongoClient, MongoClient>(sp =>
 {
