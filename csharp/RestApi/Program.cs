6154bdb34febb7d3d4e6cedf2ef23cff8b6df18e
Mudassar Majeed <mudassarm30@gmail.com>
2023-11-27 01:09:00 +0100

    Fixed reference

--- csharp/RestApi/Program.cs
+++ csharp/RestApi/Program.cs
@@ -9,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+builder.Services.AddAWSLambdaHosting(LambdaEventSource.HttpApi);
 builder.Services.Configure<MongoDBConfig>(builder.Configuration.GetSection("MongoDB"));
 builder.Services.AddSingleton<IMongoClient, MongoClient>(sp =>
 {
